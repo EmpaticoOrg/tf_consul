50175a47fc0f43352a6611187beaf4c286ab8880
James Turnbull <james@lovedthanlost.net>
2016-12-15 01:08:36 -0500

    Fixed variables

--- interface.tf
+++ interface.tf
@@ -87,3 +87,14 @@ output "launch_configuration" {
 output "asg_name" {
   value = "${aws_autoscaling_group.consul.id}"
 }
+
+output "consul_primary_server_address" {
+  value = "${aws_instance.server.0.public_dns}"
+}
+
+output "consul_server_addresses" {
+  value = ["${aws_instance.server.*.public_dns}"]
+
+output "consul_client_addresses" {
+  value = ["${aws_instance.client.*.public_dns}"]
+}
