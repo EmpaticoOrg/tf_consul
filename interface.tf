8b94f00536a53974f07122945103c25ecfd8a19d
James Turnbull <james@lovedthanlost.net>
2016-12-16 00:58:31 -0500

    Removed outputs

--- interface.tf
+++ interface.tf
@@ -80,14 +80,3 @@ variable "encryption_key" {
   description = "Encryption key. 16-bytes & Base64-encoded. Best generated with the consul keygen command."
 }
 
-output "consul_primary_server_address" {
-  value = "${aws_instance.server.0.public_dns}"
-}
-
-output "consul_server_addresses" {
-  value = ["${aws_instance.server.*.public_dns}"]
-}
-
-output "consul_client_addresses" {
-  value = ["${aws_instance.client.*.public_dns}"]
-}
