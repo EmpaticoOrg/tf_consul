39628cbdd085a36e23829d7ce2600356dc8a5c01
James Turnbull <james@lovedthanlost.net>
2016-12-31 10:15:24 -0500

    Removed ICMP

--- main.tf
+++ main.tf
@@ -177,13 +177,6 @@ resource "aws_security_group" "consul_inbound_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8
-    to_port     = 0
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
