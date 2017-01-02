6db3788e676f348f8fccdbfc2066ca2a0111243d
James Turnbull <james@lovedthanlost.net>
2017-01-02 02:03:31 -0500

    Fixed mv

--- main.tf
+++ main.tf
@@ -43,9 +43,9 @@ resource "aws_elb" "consul" {
   }
 
   access_logs {
-    bucket = "empatico-elb-logs"
+    bucket        = "empatico-elb-logs"
     bucket_prefix = "${var.environment}-${var.role}"
-    enabled = true
+    enabled       = true
   }
 
   lifecycle {
@@ -101,14 +101,14 @@ resource "aws_iam_instance_profile" "consul" {
 }
 
 resource "aws_autoscaling_group" "consul" {
-  name                      = "${aws_launch_configuration.consul.name}-asg"
-  launch_configuration      = "${aws_launch_configuration.consul.name}"
-  desired_capacity          = 5
-  min_size                  = 3
-  max_size                  = 5
-  min_elb_capacity          = 3
-  load_balancers            = ["${aws_elb.consul.id}"]
-  vpc_zone_identifier       = ["${var.public_subnet_id}"]
+  name                 = "${aws_launch_configuration.consul.name}-asg"
+  launch_configuration = "${aws_launch_configuration.consul.name}"
+  desired_capacity     = 5
+  min_size             = 3
+  max_size             = 5
+  min_elb_capacity     = 3
+  load_balancers       = ["${aws_elb.consul.id}"]
+  vpc_zone_identifier  = ["${var.public_subnet_id}"]
 
   lifecycle {
     create_before_destroy = true
